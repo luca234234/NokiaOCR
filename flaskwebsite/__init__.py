cfb7194a6329356c7e9511c0abbbd7b68a2cee39
luca234234 <posta.verde.21@gmail.com>
2024-04-03 22:03:40 +0300

    bugfixes

--- flaskwebsite/__init__.py
+++ flaskwebsite/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from authlib.integrations.flask_client import OAuth
-from config import Config
+from flaskwebsite.config import Config
 from flask_mail import Mail
 
 db = SQLAlchemy()
